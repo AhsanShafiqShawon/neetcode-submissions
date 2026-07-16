c65573d8e9e8b30c55218e2ac2286368fe9b1ed4
Ahsan Shafiq Shawon <ahsan.shafiq.shawon@gmail.com>
2026-07-16 14:31:01 +0700

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int ans = 0, n = nums.size();
+        for(int i = 0; i < n; i++) ans += nums[i] - i;
+        return n - ans;
+    }
+};
